252a062e41b1d48b84805518e45375e26089b0c7
Simon Egersand <simon.egersand@weknowit.nu>
2014-10-07 11:02:16 +0200

    added comments

--- src/main.lua
+++ src/main.lua
@@ -1,30 +1,39 @@
+--- Main menu
+-- 
+-- The main menu for the application GetSmart
+
+
+-- Require the grafics library and setting the background color
 gfx = require "gfx"
 gfx.screen:clear({255,255,255}) --RGB
 gfx.update()
 
-png_circle_width = 160
-png_circle_height = 160
+-- All menu items as .png pictures as transparent background
 png_circles = { game1 = 'images/menu-math.png',
 	game2 = 'images/menu-flags.png',
 	game3 = 'images/menu-memory.png',
 	game4 = 'images/menu-spelling.png',
 }
+
+-- Logotype as .png pictuere with transparent background with width
 png_logo_width = 447
 png_logo = 'images/logo.png'
 
+
+-- Directory of artwork
 dir = './'
 
+-- Prints main menu
 function printMenuCircles()
 
 	local toScreen = nil
-	
 	local gameCounter = 1
 
 	-- Prints menu items
 	for i = 50, 1000, 250 do
 		toScreen = gfx.loadpng(dir..png_circles['game'..gameCounter])
 		printCircle(toScreen, i, 450)
-		gameCounter = gameCounter + 1
+		gameCounter = gameCounter+1
 	end
 	
 	-- Prints logo
@@ -35,18 +44,18 @@ function printMenuCircles()
 
 end
 
+-- Print circle according to img, x and y values
 function printCircle(img, xIn, yIn)
 
 	gfx.screen:copyfrom(img, nil, {x=xIn, y=yIn})
 
 end
 
+-- Main function that runs the program
 local function main()
 
   printMenuCircles()
   
-
-
 end
 
 main()
