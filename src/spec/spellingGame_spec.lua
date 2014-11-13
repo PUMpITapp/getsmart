f78c9f2d539a76769b003cb2b987bc6470ddd89f
Simon Lindgren <simli357@student.liu.se>
2014-11-13 12:16:29 +0100

    Fixed splitIntoWordParts and wrote tests for it

--- src/spec/spellingGame_spec.lua
+++ src/spec/spellingGame_spec.lua
@@ -1,7 +1,6 @@
 require "spellingGame"
 
-describe('Testing init: ', function ()
-	
+describe('Testing init: ', function ()	
 	-- The following test checks that the words in the answer table has the 
 	-- same number of intervalls as alternative sets. 
 	-- It loops through answer table
@@ -34,4 +33,51 @@ describe('Testing init: ', function ()
 		got = images.colors
 		assert.are_not.same(notExpected,got)
 	end)
+end)
+
+--[[
+	it('', function ()
+		expected = 
+		got = 
+		assert.are.equals(expected, got)
+	end)
+]]
+
+describe('Testing generateQuestion: ', function ()
+	init() 
+  	wordArray = selectRandomWord()
+  	question = generateQuestion(wordArray)
+
+	it('Checks that 4 alterantives are provided to the user for all intervalls',function ()
+		expected = #question[2]
+		got = 0
+		for i=1, #question[2] do
+			if #question[2][1] == 4 then
+				got = got + 1
+			end
+		end
+		assert.are.equals(expected, got)
+	end)
+
+end)
+
+describe('Testing splitIntoWordParts: ', function ()
+	it('Splitting the word: Test, intervall = [3,4]. Expecting 1 word part', function ()
+		expected = 1
+		got = #splitIntoWordParts('test',{{3,4}})
+		assert.are.equals(expected, got)
+	end)
+
+	it('Splitting the word: Test, intervall = [2,3]. Expecting 2 word parts', function ()
+		expected = 2
+		got = #splitIntoWordParts('test',{{2,3}})
+		assert.are.equals(expected, got)
+	end)
+
+	it('Splitting the word: Test, intervall = [1,2]. Expecting 1 word part', function ()
+		expected = 1
+		got = #splitIntoWordParts('test',{{1,2}})
+		assert.are.equals(expected, got)
+	end)	
+
 end)
