ad976bd0f7d97c3580334a1f063bbf77712bbf61
Mikael Malmstrom <mikael.malmstrom.79@gmail.com>
2014-11-13 08:37:33 +0100

    Changed how to initiate profiles.lua. Now just run the command 'lua profiles_init.lua' to initiate the table.

--- src/profiles.lua
+++ src/profiles.lua
@@ -1,73 +1,73 @@
 return {
 -- Table: {1}
 {
-   ["player2"]={2},
-   ["player3"]={3},
-   ["player1"]={4},
-   ["player4"]={5},
+   ["player3"]={2},
+   ["player4"]={3},
+   ["player2"]={4},
+   ["player1"]={5},
 },
 -- Table: {2}
 {
-   ["name"]="Frida",
    ["spellingGame"]=1,
+   ["name"]="",
    ["memoryGame"]=1,
    ["mathGame"]={6},
    ["geographyGame"]=1,
 },
 -- Table: {3}
 {
-   ["name"]="",
    ["spellingGame"]=1,
+   ["name"]="",
    ["memoryGame"]=1,
    ["mathGame"]={7},
    ["geographyGame"]=1,
 },
 -- Table: {4}
 {
-   ["name"]="Per",
    ["spellingGame"]=1,
+   ["name"]="Frida",
    ["memoryGame"]=1,
    ["mathGame"]={8},
    ["geographyGame"]=1,
 },
 -- Table: {5}
 {
-   ["name"]="",
    ["spellingGame"]=1,
+   ["name"]="Per",
    ["memoryGame"]=1,
    ["mathGame"]={9},
    ["geographyGame"]=1,
 },
 -- Table: {6}
 {
-   ["userLevel"]=1,
-   ["divisionPoints"]=0,
    ["subtractionPoints"]=0,
-   ["multiplicationPoints"]=0,
    ["additionPoints"]=0,
+   ["userLevel"]=1,
+   ["multiplicationPoints"]=0,
+   ["divisionPoints"]=0,
 },
 -- Table: {7}
 {
-   ["userLevel"]=1,
-   ["divisionPoints"]=0,
    ["subtractionPoints"]=0,
-   ["multiplicationPoints"]=0,
    ["additionPoints"]=0,
+   ["userLevel"]=1,
+   ["multiplicationPoints"]=0,
+   ["divisionPoints"]=0,
 },
 -- Table: {8}
 {
-   ["userLevel"]=1,
-   ["divisionPoints"]=0,
    ["subtractionPoints"]=0,
+   ["additionPoints"]=0,
+   ["userLevel"]=1,
    ["multiplicationPoints"]=0,
-   ["additionPoints"]=4,
+   ["divisionPoints"]=0,
 },
 -- Table: {9}
 {
-   ["userLevel"]=1,
-   ["divisionPoints"]=0,
    ["subtractionPoints"]=0,
-   ["multiplicationPoints"]=0,
    ["additionPoints"]=0,
+   ["userLevel"]=1,
+   ["multiplicationPoints"]=0,
+   ["divisionPoints"]=0,
 },
 }
