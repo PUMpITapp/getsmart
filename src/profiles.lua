0557390000636288e4f7ee20ba3109537f2ade4c
Simon Egersand <s.egersand@gmail.com>
2014-11-18 11:04:50 +0100

    Changes

--- src/profiles.lua
+++ src/profiles.lua
@@ -1,73 +1,73 @@
 return {
 -- Table: {1}
 {
-   ["player2"]={2},
-   ["player4"]={3},
-   ["player3"]={4},
-   ["player1"]={5},
+   ["player3"]={2},
+   ["player2"]={3},
+   ["player1"]={4},
+   ["player4"]={5},
 },
 -- Table: {2}
 {
-   ["geographyGame"]=1,
    ["spellingGame"]=1,
-   ["memoryGame"]=1,
-   ["name"]="Frida",
+   ["name"]="",
+   ["geographyGame"]=1,
    ["mathGame"]={6},
+   ["memoryGame"]=1,
 },
 -- Table: {3}
 {
-   ["geographyGame"]=1,
    ["spellingGame"]=1,
-   ["memoryGame"]=1,
-   ["name"]="",
+   ["name"]="Frida",
+   ["geographyGame"]=1,
    ["mathGame"]={7},
+   ["memoryGame"]=1,
 },
 -- Table: {4}
 {
+   ["spellingGame"]=56,
+   ["name"]="Per",
    ["geographyGame"]=1,
-   ["spellingGame"]=1,
-   ["memoryGame"]=1,
-   ["name"]="",
    ["mathGame"]={8},
+   ["memoryGame"]=1,
 },
 -- Table: {5}
 {
+   ["spellingGame"]=1,
+   ["name"]="",
    ["geographyGame"]=1,
-   ["spellingGame"]=55,
-   ["memoryGame"]=1,
-   ["name"]="Per",
    ["mathGame"]={9},
+   ["memoryGame"]=1,
 },
 -- Table: {6}
 {
-   ["multiplicationPoints"]=1,
    ["additionPoints"]=1,
    ["subtractionPoints"]=1,
-   ["divisionPoints"]=1,
+   ["multiplicationPoints"]=1,
    ["userLevel"]=1,
+   ["divisionPoints"]=1,
 },
 -- Table: {7}
 {
-   ["multiplicationPoints"]=1,
    ["additionPoints"]=1,
    ["subtractionPoints"]=1,
-   ["divisionPoints"]=1,
+   ["multiplicationPoints"]=1,
    ["userLevel"]=1,
+   ["divisionPoints"]=1,
 },
 -- Table: {8}
 {
-   ["multiplicationPoints"]=1,
-   ["additionPoints"]=1,
-   ["subtractionPoints"]=1,
-   ["divisionPoints"]=1,
-   ["userLevel"]=1,
+   ["additionPoints"]=63,
+   ["subtractionPoints"]=10,
+   ["multiplicationPoints"]=14,
+   ["userLevel"]=11,
+   ["divisionPoints"]=14,
 },
 -- Table: {9}
 {
-   ["multiplicationPoints"]=14,
-   ["additionPoints"]=62,
-   ["subtractionPoints"]=10,
-   ["divisionPoints"]=14,
-   ["userLevel"]=11,
+   ["additionPoints"]=1,
+   ["subtractionPoints"]=1,
+   ["multiplicationPoints"]=1,
+   ["userLevel"]=1,
+   ["divisionPoints"]=1,
 },
 }
