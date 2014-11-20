e8cbe59a3b559bc1ab8a7118db999da7bc20ebc4
Arthur Rapaport <a_rapaport@hotmail.com>
2014-11-20 12:04:28 +0100

    Started working on flags

--- src/flagGame.lua
+++ src/flagGame.lua
@@ -8,6 +8,12 @@ profileHandler = require 'profileHandler'
 
 require 'helpers'
 local randomSeed = os.time()
+local circleDiameter = 80;
+
+local screen = {
+  Width = gfx.screen:get_width(),
+  Height = gfx.screen:get_height()
+}
 
 local images = {
   Colors = gfx.loadpng('images/color_choices.png'),
@@ -27,8 +33,8 @@ function printFlag(countryId)
   flagSurface:copyfrom(images.Flags, flagDimensions, true)
 
   local flagScreenPosition = {
-    x = gfx.screen:get_width() / 4 - flagDimensions.w / 2,
-    y = gfx.screen:get_height() / 2 - flagDimensions.h / 2,
+    x = screen.Width / 4 - flagDimensions.w / 2,
+    y = screen.Height / 2 - flagDimensions.h / 2,
     w = flagDimensions.w,
     h = flagDimensions.h
   }
@@ -37,25 +43,98 @@ function printFlag(countryId)
   gfx.update()
 end
 
-function printAnswers(countryId)
-  local answers = {
-    flags[countryId].country
+function printAnswers(answers)
+
+  local font = {
+    face = 'lato',
+    color = 'black',
+    size = 'large',
+    height = text.getFontHeight('lato', 'large')
   }
 
-  while (table.maxn(answers) < 4) do
-    newCountryId = getRandomCountryId()
+  local textPosition = {
+    x = screen.Width / 2 + circleDiameter * 3 / 2,
+    y = screen.Height / 8 - font.height / 2,
+    w = screen.Width / 2,
+    h = screen.Height / 4
+  }
 
-    if (not table.contains(answers, newCountryId)) then
-        table.insert(answers, newCountryId)
-        print(tostring(newCountryId) .. ' was inserted!')
+  for i = 1, 4 do
+    text.print(gfx.screen, font.face, font.color, font.size, answers[i], textPosition.x, textPosition.y, textPosition.w, textPosition.h)
+    textPosition.y = textPosition.y + screen.Height / 4
+  end
+
+end
+
+function createColoredCircles()
+
+  -- Positions in the circle sprite.
+  local xs = 40  -- x starting coordinate
+  local y = xs  -- y position
+  local d = 160 -- diameter of circle
+
+  local cutOut = {
+    red    = {x = xs,         y = y, w = d, h = d},
+    yellow = {x = xs + d,     y = y, w = d, h = d},
+    blue   = {x = xs + d * 2, y = y, w = d, h = d},
+    green  = {x = xs + d * 3, y = y, w = d, h = d}
+  }
+
+  circle = {
+    red    = gfx.new_surface(cutOut.red.w, cutOut.red.h),
+    green  = gfx.new_surface(cutOut.green.w, cutOut.green.h),
+    yellow = gfx.new_surface(cutOut.yellow.w, cutOut.yellow.h),
+    blue   = gfx.new_surface(cutOut.blue.w, cutOut.blue.h)
+  }
+
+  circle.red:copyfrom(images.Colors, cutOut.red, true)
+  circle.green:copyfrom(images.Colors, cutOut.green, true)
+  circle.yellow:copyfrom(images.Colors, cutOut.yellow, true)
+  circle.blue:copyfrom(images.Colors, cutOut.blue, true)
+
+end
+
+function placeAnswerCircles()
+
+  local circlePosition = {
+    x = screen.Width / 2,
+    y = screen.Height / 8 - circleDiameter / 2,
+    w = circleDiameter,
+    h = circleDiameter
+  }
+
+  local circleColors = {
+    'red',
+    'green',
+    'yellow',
+    'blue'
+  }
+
+  for i = 1,4 do
+    gfx.screen:copyfrom(circle[circleColors[i]], nil, circlePosition, true)
+    circlePosition.y = circlePosition.y + screen.Height / 4
+  end
+
+  gfx.update()
+end
+
+function generateAnswers(correctCountryId)
+  -- Initialize the answers array and populate it with the correct answer
+  local answers = {
+    flags[correctCountryId].country
+  }
+
+  -- Generate random incorrect answers
+  while (table.maxn(answers) < 4) do
+    local newCountryId = getRandomCountryId()
+    local newAnswer = flags[newCountryId].country
+
+    if (not table.contains(answers, newAnswer)) then
+        table.insert(answers, newAnswer)
     end
   end
 
-  for i = 1,4 do
-    print(answers[i])
-  end
-
-
+  return shuffle(answers)
 end
 
 function checkAnswer(userAnswer, countryId) end
@@ -67,9 +146,22 @@ function getRandomCountryId()
   return randomCountryId
 end
 
-printFlag(6)
---for i = 1,10 do
---  print(getRandomCountryId())
---end
+function init()
+  -- Set background
+  gfx.screen:clear({122,219,228})
+  gfx.update()
+end
 
-printAnswers(1)
+function generateQuestion()
+  -- bestäm random land
+
+end
+
+init()
+printFlag(5)
+
+
+printAnswers(generateAnswers(5))
+
+createColoredCircles()
+placeAnswerCircles()
--- src/helpers.lua
+++ src/helpers.lua
@@ -1,5 +1,5 @@
 --- Check if a table contains a given element
--- @param table The table to check in
+-- @param table #table The table to check in
 -- @param element The element to check for
 function table.contains(table, element)
   for _, value in pairs(table) do
@@ -8,4 +8,15 @@ function table.contains(table, element)
     end
   end
   return false
+end
+
+--- Shuffles an array
+-- @param #table array Table to shuffle
+function shuffle(array)
+  local n, random, j = #array, math.random
+  for i=1, n do
+    j,k = random(n), random(n)
+    array[j],array[k] = array[k],array[j]
+  end
+  return array
 end
