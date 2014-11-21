4f721bc5be07bf3b5f03a426a8e9b3fe49078efe
Albin <albot609@student.liu.se>
2014-11-21 15:13:53 +0100

    tests for flag game

--- src/spec/flagGame_spec.lua
+++ src/spec/flagGame_spec.lua
@@ -1,9 +1,12 @@
 require "flagGame"
 
---[[describe('testing checkAnswer(userAnswer) ', function ( ... )
+describe('testing checkAnswer(userAnswer) ', function ( ... )
 	it('comparing userAnswer to correctAnswer', function ( ... )
 	
+correctAnswer == 1
+checkAnswer(1)
 
+assert.are.true(true, answerState)
 	
 	end)
-end)--]]
+end)
