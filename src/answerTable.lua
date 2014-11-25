016706fe5df32a55047c65717bcebc3dfe57d9bc
Albin <albot609@student.liu.se>
2014-11-25 12:56:02 +0100

    moving answerTable to extern file

---
+++ src/answerTable.lua
@@ -0,0 +1,13 @@
+return {
+            {'Yesterday',{{2,3},{6,7}},{{'es','is','ez','oys'},{'rd','d','rp','ld'}}},
+            {'Artichoke',{{3,4}},{{'ti','tie','te','to'}}},
+            {'Scrummaster',{{2,3},{5,6}},{{'cr','ckr','kr','crr'},{'mm','m','me','mm'}}},
+            {'acquire',{{2,3}},{{'cq','cc','q','k'}}},
+            {'rhythm',{{2,3}},{{'hy','y','yi','hu'}}},
+            {'vacuum',{{3,4}},{{'cu','k','c','cc'}}},
+    
+            {'questionnaire',{{2,3},{9,10}},{{'ue','oe','u','e'},{'na','a','no','n'}}},
+            {'playwright',{{5,6}},{{'wr','r','whr','ir'}}},
+            {'neighbor',{{3,5}},{{'igh','yh','ih','ygh'}}}
+
+  }
--- src/spellingGame.lua
+++ src/spellingGame.lua
@@ -35,17 +35,7 @@ player = ...
 
 ---Initiating all global variables
 function init()
-  answerTable = {
-    {'Yesterday',{{2,3},{6,7}},{{'es','is','ez','oys'},{'rd','d','rp','ld'}}},
-    {'Artichoke',{{3,4}},{{'ti','tie','te','to'}}},
-    {'Scrummaster',{{2,3},{5,6}},{{'cr','ckr','kr','crr'},{'mm','m','me','mm'}}},
-    {'acquire',{{2,3}},{{'cq','cc','q','k'}}},
-    {'rhythm',{{2,3}},{{'hy','y','yi','hu'}}},
-    {'vacuum',{{3,4}},{{'cu','k','c','cc'}}},
-    {'questionnaire',{{2,3},{9,10}},{{'ue','oe','u','e'},{'na','a','no','n'}}},
-    {'playwright',{{5,6}},{{'wr','r','whr','ir'}}},
-    {'neighbor',{{3,5}},{{'igh','yh','ih','ygh'}}}
-  }
+  answerTable = require 'answerTable'
   rightAlternatives = {} 
   images = {['colors'] = "images/color_choices.png"} 
   inFocus = nil
