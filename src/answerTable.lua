7fc049b1be2905d35088179328857580bd2f7b42
Albin <albot609@student.liu.se>
2014-11-26 12:32:29 +0100

    adding words to answerTable, spellingGame

--- src/answerTable.lua
+++ src/answerTable.lua
@@ -1,40 +1,41 @@
 return {
 		{
-		            {'Yesterday',{{2,3},{6,7}},{{'es','is','ez','oys'},{'rd','d','rp','ld'}}},
-		            {'Artichoke',{{3,4}},{{'ti','tie','te','to'}}},
-		            {'Scrummaster',{{2,3},{5,6}},{{'cr','ckr','kr','crr'},{'mm','m','me','mm'}}},
-		            {'Acquire',{{2,3}},{{'cq','cc','q','k'}}},
-		            {'Rhythm',{{2,3}},{{'hy','y','yi','hu'}}},
-		            {'Vacuum',{{3,4}},{{'cu','k','c','cc'}}}
+					{'House',{{2,3}},{{'ou','uo','au','ao'}}},
+					{'Mother',{{3,4}},{{'th','t','dh','ht'}}},
+					{'Father',{{3,4}},{{'th','t','dh','ht'}}},
+					{'Sister',{{3,4}},{{'st','sst','sth','sh'}}},
+					{'Brother',{{4,5}},{{'th','h','t','ht'}}}
+
+
+
             },
             
 			{
-		            {'Questionnaire',{{2,3},{9,10}},{{'ue','oe','u','e'},{'na','a','no','n'}}},
+		            {'Acquire',{{2,3}},{{'cq','cc','q','k'}}},
+		            {'Vacuum',{{3,4}},{{'cu','k','c','cc'}}},
+		            {'Rhythm',{{2,3}},{{'hy','y','yi','hu'}}},
 		            {'Playwright',{{5,6}},{{'wr','r','whr','ir'}}},
 		            {'Neighbor',{{3,5}},{{'igh','yh','ih','ygh'}}}
 			},
 			
 			{
+		            {'Scrummaster',{{2,3},{5,6}},{{'cr','ckr','kr','crr'},{'mm','m','me','mm'}}},
+		            {'Yesterday',{{2,3},{6,7}},{{'es','is','ez','oys'},{'rd','d','rp','ld'}}},
+		            {'Artichoke',{{3,4}},{{'ti','tie','te','to'}}},
 		            {'Questionnaire',{{2,3},{9,10}},{{'ue','oe','u','e'},{'na','a','no','n'}}},
-		            {'Playwright',{{5,6}},{{'wr','r','whr','ir'}}},
-		            {'Neighbor',{{3,5}},{{'igh','yh','ih','ygh'}}}
+					{'Requirement',{{3,4},{7,8}},{{'qu','q','eq','qa'},{'em','m','emm','im'}}}
+
 			},
 			
 			{
-		            {'Questionnaire',{{2,3},{9,10}},{{'ue','oe','u','e'},{'na','a','no','n'}}},
-		            {'Playwright',{{5,6}},{{'wr','r','whr','ir'}}},
-		            {'Neighbor',{{3,5}},{{'igh','yh','ih','ygh'}}}
-			},
-			
-			{
-		            {'Questionnaire',{{2,3},{9,10}},{{'ue','oe','u','e'},{'na','a','no','n'}}},
-		            {'Playwright',{{5,6}},{{'wr','r','whr','ir'}}},
-		            {'Neighbor',{{3,5}},{{'igh','yh','ih','ygh'}}}
-			},
-			
-			{
-		            {'Questionnaire',{{2,3},{9,10}},{{'ue','oe','u','e'},{'na','a','no','n'}}},
-		            {'Playwright',{{5,6}},{{'wr','r','whr','ir'}}},
-		            {'Neighbor',{{3,5}},{{'igh','yh','ih','ygh'}}}
+		            {'Accommodate',{{2,3},{5,6}},{{'cc','c','cq','ck'},{'mm','m','em','am'}}},
+		            {'Conscientious',{{4,5},{9,10}},{{'sc','ti','sh','c'},{'ti','sc','sh','ta'}}},
+		            {'Pronunciation',{{5,6},{10,11}},{{'un','an','en','in'},{'ti','ch','sh','te'}}},
+		            {'Misspelled',{{3,4},{7,8}},{{'ss','s','es','se'},{'ll','l','el','le'}}},
+		            {'Presbyterian',{{4,6}},{{'sby','sbe','sbi','cby'}}}
+
+
 			}
+			
+			
   }
