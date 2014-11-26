416045de684de59e70d4ee4d325eb4c41a912a6a
Julia Thudén <julth357@student.liu.se>
2014-11-26 12:38:42 +0100

    added words

--- src/answerTable.lua
+++ src/answerTable.lua
@@ -1,40 +1,52 @@
 return {
+---Level 0/1
 		{
-		            {'Yesterday',{{2,3},{6,7}},{{'es','is','ez','oys'},{'rd','d','rp','ld'}}},
-		            {'Artichoke',{{3,4}},{{'ti','tie','te','to'}}},
+		            
+		            {'Dog',{{2,2}},{{'o','ou','uo','a'}}},
+		            {'Boat',{{2,3}},{{'oa','o','ou','oo'}}},
+		            {'House',{{2,3}},{{'ou','o','uo','oo'}}},
+		            {'Apple',{{2,3}},{{'pp','p','pe','pl'}}},
+		            {'Pen',{{2,2}},{{'e','en','ee','ea'}}},
+		            {'Clock',{{3,4}},{{'oc','o','oo','ok'}}},
+		            {'Play',{{3,3}},{{'a','la','e','ei'}}}
+		           
+            },
+---Level 2            
+			{
+		           	{'Coffee',{{3,4}},{{'ff','of','f','uf'}}},
+		           	{'Queen',{{2,3}},{{'ue','u','oe','o'}}},
+		           	{'Food',{{2,3}},{{'oo','ou','uo','o'}}},
+		           	{'Water',{{2,2}},{{'a','o','oo','ae'}}},
+		           	{'Mobile',{{2,2}},{{'o','ou','oo','a'}}}
+
+			},
+---Level 3			
+			{
+		            {'Guitar',{{2,3}},{{'ui','uu','u','i'}}},
+		            {'Question',{{2,3}},{{'ue','oe','u','e'}}},
+		            {'Difficult',{{3,4},{7,7}},{{'ff','f','fu','if'},{'u','o','uu','oo'}}},
+		            {'Acceptable',{{2,3},{7,7}},{{'cc','c','ce','e'},{'a','ae','e','ea'}}},
+		            {'Committed',{{3,4},{6,7}},{{'mm','om','m','o'},{'tt','t','it','i'}}},
+		            {'Intelligence',{{5,6},{9,9}},{{'ll','l','el','li'},{'e','ee','a','ie'}}}
+			},
+--- Level 4			
+			{
+		           	{'Artichoke',{{3,4}},{{'ti','tie','te','to'}}},
 		            {'Scrummaster',{{2,3},{5,6}},{{'cr','ckr','kr','crr'},{'mm','m','me','mm'}}},
 		            {'Acquire',{{2,3}},{{'cq','cc','q','k'}}},
 		            {'Rhythm',{{2,3}},{{'hy','y','yi','hu'}}},
 		            {'Vacuum',{{3,4}},{{'cu','k','c','cc'}}}
-            },
-            
-			{
-		            {'Questionnaire',{{2,3},{9,10}},{{'ue','oe','u','e'},{'na','a','no','n'}}},
-		            {'Playwright',{{5,6}},{{'wr','r','whr','ir'}}},
-		            {'Neighbor',{{3,5}},{{'igh','yh','ih','ygh'}}}
+		           
 			},
-			
+--- Level 5			
 			{
-		            {'Questionnaire',{{2,3},{9,10}},{{'ue','oe','u','e'},{'na','a','no','n'}}},
+		          	{'Questionnaire',{{2,3},{9,10}},{{'ue','oe','u','e'},{'na','a','no','n'}}},
 		            {'Playwright',{{5,6}},{{'wr','r','whr','ir'}}},
-		            {'Neighbor',{{3,5}},{{'igh','yh','ih','ygh'}}}
+		            {'Neighbor',{{3,5}},{{'igh','yh','ih','ygh'}}},
+		            {'Hierarchy',{{2,3},{7,8}},{{'ie','i','e','ee'},{'ch','c','k','ck'}}}
 			},
-			
+--- Level 6			
 			{
-		            {'Questionnaire',{{2,3},{9,10}},{{'ue','oe','u','e'},{'na','a','no','n'}}},
-		            {'Playwright',{{5,6}},{{'wr','r','whr','ir'}}},
-		            {'Neighbor',{{3,5}},{{'igh','yh','ih','ygh'}}}
-			},
-			
-			{
-		            {'Questionnaire',{{2,3},{9,10}},{{'ue','oe','u','e'},{'na','a','no','n'}}},
-		            {'Playwright',{{5,6}},{{'wr','r','whr','ir'}}},
-		            {'Neighbor',{{3,5}},{{'igh','yh','ih','ygh'}}}
-			},
-			
-			{
-		            {'Questionnaire',{{2,3},{9,10}},{{'ue','oe','u','e'},{'na','a','no','n'}}},
-		            {'Playwright',{{5,6}},{{'wr','r','whr','ir'}}},
-		            {'Neighbor',{{3,5}},{{'igh','yh','ih','ygh'}}}
+		            {'Questionnaire',{{2,3},{9,10}},{{'ue','oe','u','e'},{'na','a','no','n'}}}
 			}
   }
