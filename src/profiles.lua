2d20fb1c7afb68d07fbba1541c54239cac89da6e
Simon Egersand <s.egersand@gmail.com>
2014-11-26 10:20:15 +0100

    Profile changes

--- src/profiles.lua
+++ src/profiles.lua
@@ -1,6 +1,7 @@
 return {
 -- Table: {1}
 {
+<<<<<<< Updated upstream
    ["player1"]={2},
    ["player4"]={3},
    ["player3"]={4},
@@ -41,6 +42,48 @@ return {
    ["memoryGame"]={20},
    ["isActive"]=0,
    ["spellingGame"]={21},
+=======
+   ["player2"]={2},
+   ["player1"]={3},
+   ["player4"]={4},
+   ["player3"]={5},
+},
+-- Table: {2}
+{
+   ["name"]="",
+   ["memoryGame"]={6},
+   ["flagGame"]={7},
+   ["spellingGame"]={8},
+   ["mathGame"]={9},
+   ["isActive"]=0,
+},
+-- Table: {3}
+{
+   ["name"]="Julbin",
+   ["memoryGame"]={10},
+   ["flagGame"]={11},
+   ["spellingGame"]={12},
+   ["mathGame"]={13},
+   ["isActive"]=1,
+},
+-- Table: {4}
+{
+   ["name"]="Jachael",
+   ["memoryGame"]={14},
+   ["flagGame"]={15},
+   ["spellingGame"]={16},
+   ["mathGame"]={17},
+   ["isActive"]=1,
+},
+-- Table: {5}
+{
+   ["name"]="Artrik",
+   ["memoryGame"]={18},
+   ["flagGame"]={19},
+   ["spellingGame"]={20},
+   ["mathGame"]={21},
+   ["isActive"]=1,
+>>>>>>> Stashed changes
 },
 -- Table: {6}
 {
@@ -49,11 +92,16 @@ return {
 },
 -- Table: {7}
 {
+<<<<<<< Updated upstream
    ["divisionPoints"]=0,
    ["subtractionPoints"]=0,
    ["userLevel"]=0,
    ["additionPoints"]=0,
    ["multiplicationPoints"]=0,
+=======
+   ["points"]=0,
+   ["userLevel"]=0,
+>>>>>>> Stashed changes
 },
 -- Table: {8}
 {
@@ -62,8 +110,11 @@ return {
 },
 -- Table: {9}
 {
-   ["points"]=0,
+   ["subtractionPoints"]=0,
+   ["multiplicationPoints"]=0,
    ["userLevel"]=0,
+   ["divisionPoints"]=0,
+   ["additionPoints"]=0,
 },
 -- Table: {10}
 {
@@ -72,21 +123,29 @@ return {
 },
 -- Table: {11}
 {
+<<<<<<< Updated upstream
    ["divisionPoints"]=0,
    ["subtractionPoints"]=0,
    ["userLevel"]=0,
    ["additionPoints"]=0,
    ["multiplicationPoints"]=0,
+=======
+   ["points"]=6,
+   ["userLevel"]=1,
+>>>>>>> Stashed changes
 },
 -- Table: {12}
 {
-   ["points"]=0,
-   ["userLevel"]=0,
+   ["points"]=9,
+   ["userLevel"]=1,
 },
 -- Table: {13}
 {
-   ["points"]=0,
+   ["subtractionPoints"]=0,
+   ["multiplicationPoints"]=0,
    ["userLevel"]=0,
+   ["divisionPoints"]=0,
+   ["additionPoints"]=0,
 },
 -- Table: {14}
 {
@@ -95,11 +154,16 @@ return {
 },
 -- Table: {15}
 {
+<<<<<<< Updated upstream
    ["divisionPoints"]=0,
    ["subtractionPoints"]=0,
    ["userLevel"]=0,
    ["additionPoints"]=0,
    ["multiplicationPoints"]=0,
+=======
+   ["points"]=6,
+   ["userLevel"]=1,
+>>>>>>> Stashed changes
 },
 -- Table: {16}
 {
@@ -108,8 +172,11 @@ return {
 },
 -- Table: {17}
 {
-   ["points"]=0,
+   ["subtractionPoints"]=0,
+   ["multiplicationPoints"]=0,
    ["userLevel"]=0,
+   ["divisionPoints"]=0,
+   ["additionPoints"]=0,
 },
 -- Table: {18}
 {
@@ -118,11 +185,16 @@ return {
 },
 -- Table: {19}
 {
+<<<<<<< Updated upstream
    ["divisionPoints"]=0,
    ["subtractionPoints"]=0,
    ["userLevel"]=0,
    ["additionPoints"]=0,
    ["multiplicationPoints"]=0,
+=======
+   ["points"]=0,
+   ["userLevel"]=0,
+>>>>>>> Stashed changes
 },
 -- Table: {20}
 {
@@ -131,7 +203,10 @@ return {
 },
 -- Table: {21}
 {
-   ["points"]=0,
+   ["subtractionPoints"]=0,
+   ["multiplicationPoints"]=0,
    ["userLevel"]=0,
+   ["divisionPoints"]=0,
+   ["additionPoints"]=0,
 },
 }
