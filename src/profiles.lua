bc57746021b72ccf59b6f78a8b499d40caf8ab76
Arthur Rapaport <a_rapaport@hotmail.com>
2014-12-03 10:52:17 +0100

    Added 6 more flags

--- src/profiles.lua
+++ src/profiles.lua
@@ -1,59 +1,59 @@
 return {
 -- Table: {1}
 {
-   ["player2"]={2},
+   ["player1"]={2},
    ["player4"]={3},
    ["player3"]={4},
-   ["player1"]={5},
+   ["player2"]={5},
 },
 -- Table: {2}
 {
-   ["name"]="",
-   ["spellingGame"]={6},
+   ["flagGame"]={6},
    ["mathGame"]={7},
    ["memoryGame"]={8},
-   ["isActive"]=0,
-   ["flagGame"]={9},
+   ["spellingGame"]={9},
+   ["name"]="Julbin",
+   ["isActive"]=1,
 },
 -- Table: {3}
 {
-   ["name"]="Jachael",
-   ["spellingGame"]={10},
+   ["flagGame"]={10},
    ["mathGame"]={11},
    ["memoryGame"]={12},
+   ["spellingGame"]={13},
+   ["name"]="Jachael",
    ["isActive"]=1,
-   ["flagGame"]={13},
 },
 -- Table: {4}
 {
-   ["name"]="Artrik",
-   ["spellingGame"]={14},
+   ["flagGame"]={14},
    ["mathGame"]={15},
    ["memoryGame"]={16},
+   ["spellingGame"]={17},
+   ["name"]="Artrik",
    ["isActive"]=1,
-   ["flagGame"]={17},
 },
 -- Table: {5}
 {
-   ["name"]="Julbin",
-   ["spellingGame"]={18},
+   ["flagGame"]={18},
    ["mathGame"]={19},
    ["memoryGame"]={20},
+   ["spellingGame"]={21},
+   ["name"]="qwer",
    ["isActive"]=1,
-   ["flagGame"]={21},
 },
 -- Table: {6}
 {
-   ["points"]=0,
-   ["userLevel"]=0,
+   ["points"]=29,
+   ["userLevel"]=2,
 },
 -- Table: {7}
 {
    ["divisionPoints"]=0,
-   ["userLevel"]=0,
-   ["additionPoints"]=0,
+   ["subtractionPoints"]=3,
+   ["userLevel"]=4,
+   ["additionPoints"]=6,
    ["multiplicationPoints"]=0,
-   ["subtractionPoints"]=0,
 },
 -- Table: {8}
 {
@@ -62,7 +62,7 @@ return {
 },
 -- Table: {9}
 {
-   ["points"]=0,
+   ["points"]=4,
    ["userLevel"]=0,
 },
 -- Table: {10}
@@ -73,10 +73,10 @@ return {
 -- Table: {11}
 {
    ["divisionPoints"]=0,
+   ["subtractionPoints"]=0,
    ["userLevel"]=0,
    ["additionPoints"]=0,
    ["multiplicationPoints"]=0,
-   ["subtractionPoints"]=0,
 },
 -- Table: {12}
 {
@@ -96,10 +96,10 @@ return {
 -- Table: {15}
 {
    ["divisionPoints"]=0,
+   ["subtractionPoints"]=0,
    ["userLevel"]=0,
    ["additionPoints"]=0,
    ["multiplicationPoints"]=0,
-   ["subtractionPoints"]=0,
 },
 -- Table: {16}
 {
@@ -113,16 +113,16 @@ return {
 },
 -- Table: {18}
 {
-   ["points"]=4,
-   ["userLevel"]=0,
+   ["points"]=14,
+   ["userLevel"]=2,
 },
 -- Table: {19}
 {
    ["divisionPoints"]=0,
-   ["userLevel"]=4,
-   ["additionPoints"]=6,
+   ["subtractionPoints"]=0,
+   ["userLevel"]=0,
+   ["additionPoints"]=0,
    ["multiplicationPoints"]=0,
-   ["subtractionPoints"]=3,
 },
 -- Table: {20}
 {
@@ -131,7 +131,7 @@ return {
 },
 -- Table: {21}
 {
-   ["points"]=22,
-   ["userLevel"]=2,
+   ["points"]=0,
+   ["userLevel"]=0,
 },
 }
